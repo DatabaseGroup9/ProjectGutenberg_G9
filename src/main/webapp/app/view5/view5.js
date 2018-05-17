6652babab5e0947f4de07a94878eeffc54fcb93d
Andreas Styltsvig <ansty93@gmail.com>
2018-05-17 09:09:50 +0200

    Removing userstory 5 link.

--- src/main/webapp/app/view5/view5.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var app = angular.module('myApp.view5', ['ngRoute'])
-
-        app.config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/view5', {
-                    templateUrl: 'app/view5/view5.html'
-                });
-            }])
