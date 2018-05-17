23b4c05246ee8b20a75c604fbc486d6445d52e29
Andreas Styltsvig <ansty93@gmail.com>
2018-05-17 15:23:16 +0200

    Cucumber now works

--- src/test/java/seleniumcucumber/Stepdefs.java
+++ src/test/java/seleniumcucumber/Stepdefs.java
@@ -5,11 +5,13 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.lang3.SystemUtils;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -23,110 +25,33 @@ import static seleniumcucumber.View1.driver;
  */
 public class Stepdefs {
 
+    private static final int WAIT_MAX = 4;
     static WebDriver driver;
 
-
-    @Given("^The city is Anderson$")
-    public void the_city_is_Anderson() throws Throwable {
-        System.setProperty("webdriver.chrome.driver", "src/test/java/seleniumcucumber/chromedriver.exe");
+    public Stepdefs() {
+        if (SystemUtils.IS_OS_WINDOWS) {
+            System.setProperty("webdriver.chrome.driver", "src/test/java/seleniumcucumber/chromedriver.exe");
+        } else if (SystemUtils.IS_OS_UNIX) {
+            System.setProperty("webdriver.chrome.driver", "src/test/java/seleniumcucumber/chromedriver_unic");
+        }
         driver = new ChromeDriver();
+    }
+
+    @Given("^The city is '(.*)'$")
+    public void the_city(String city) throws Throwable {
         driver.get("http://localhost:8080/dbtest/#!/view1");
+
+    }
+
+    @When("^Enter '(.*)' and choose '(.*)'$")
+    public void enter_city_and_choose_db(String city, String database) throws Throwable {
         
     }
-//
-//    @When("^Enter the city Anderson and stub$")
-//    public void enter_the_city_Anderson_and_stub(String city, String database) throws Throwable {
-//        
-//        WebElement select_field = driver.findElement(By.id("select_db_dropdown"));
-//        WebElement cityfield = driver.findElement(By.id("city"));
-//        WebElement submit = driver.findElement(By.id("submit"));
-//        Select dropdown = new Select(select_field);
-//        dropdown.selectByValue(database);
-//        String input = city;
-//        cityfield.sendKeys(input);
-//        submit.click();
-//        TimeUnit.SECONDS.sleep(2);
-//    }
-//
-//    @Then("^I should get success$")
-//    public void i_should_get_success() throws Throwable {
-//        int expectedSize = 7;
-//        
-//        WebElement bookstable = driver.findElement(By.id("books"));
-//        WebElement body = bookstable.findElement(By.tagName("tbody"));
-//        List<WebElement> rowList = body.findElements(By.tagName("tr"));
-//        Assert.assertThat(7, is(rowList.size()));
-//    }
-//
-//    @Given("^The city is $")
-//    public void the_city_is() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
-//
-//    @When("^Enter the city  and stub$")
-//    public void enter_the_city_and_stub() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
-//
-//    @Then("^I should get Invalid Input$")
-//    public void i_should_get_Invalid_Input() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
-//
-//    @Given("^The city is Lyngby$")
-//    public void the_city_is_Lyngby() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
-//
-//    @When("^Enter the city Lyngby and stub$")
-//    public void enter_the_city_Lyngby_and_stub() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
-//
-//    @Then("^I should get No Book Found$")
-//    public void i_should_get_No_Book_Found() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
-//
-//    @When("^Enter the city Anderson and mongodb$")
-//    public void enter_the_city_Anderson_and_mongodb() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
-//
-//    @When("^Enter the city  and mongodb$")
-//    public void enter_the_city_and_mongodb() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
-//
-//    @When("^Enter the city Lyngby and mongodb$")
-//    public void enter_the_city_Lyngby_and_mongodb() throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
-//
-//    @When("^Enter the city Anderson and neo(\\d+)j$")
-//    public void enter_the_city_Anderson_and_neo_j(int arg1) throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
-//
-//    @When("^Enter the city  and neo(\\d+)j$")
-//    public void enter_the_city_and_neo_j(int arg1) throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
-//
-//    @When("^Enter the city Lyngby and neo(\\d+)j$")
-//    public void enter_the_city_Lyngby_and_neo_j(int arg1) throws Throwable {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
-//    }
+
+
+    @Then("^I should get '(.*)'$")
+    public void i_should_get_success(String page) throws Throwable {
+       
+    }
+
 }
--- src/test/resources/seleniumcucumber/get_books_by_city.feature
+++ src/test/resources/seleniumcucumber/get_books_by_city.feature
@@ -2,12 +2,13 @@ Feature: Get Books By City
   Return list of books
 
   Scenario Outline: Enter Valid City
-    Given The city is <city>
-    When Enter the city <city> and <database>
-    Then I should get <page>
-
+    Given The city is '<city>'
+    When Enter '<city>' and choose '<database>'
+    Then I should get '<page>'
 
 Examples:
     | city     | page            | database |
     | Anderson | success         | stub     |
+    | Anderson | success         | mongodb  |
+    | Anderson | success         | neo4j    |
 
